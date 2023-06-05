fb96902c1d2ce3ae584364fe8bc482a41b077581
kOsari5948 <kmc5948@naver.com>
2023-06-05 15:05:33 +0900

    sql_setting

--- backend/config/runQerr.js
+++ backend/config/runQerr.js
@@ -2,8 +2,7 @@ const mysql = require('./runQuery');
 const logger = require('./winton');
 async function runQerr(sql) {
   try{
-    await mysql(sql_staring);
-    return "sucess";
+    return await mysql(sql_staring);
   }catch(err){
     logger.error('fail query :' + sql_staring);
     return "fail";
