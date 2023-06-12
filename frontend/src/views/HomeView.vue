fa55811aca64d5073bddabf3eeb5bdce7e5686b1
kOsari5948 <kmc5948@naver.com>
2023-06-12 16:53:52 +0900

    fix-time

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -138,10 +138,10 @@ export default {
 
       var hours = ('0' + today.getHours()).slice(-2);
       var minutes = ('0' + today.getMinutes()).slice(-2);
-      var seconds = ('0' + (today.getSeconds()+3)).slice(-2);
+      var seconds = ('0' + (today.getSeconds()+5)).slice(-2);
+      var seconds_2 = ('0' + (today.getSeconds())).slice(-2);
 
       var timeString = hours + ':' + minutes + ':' + seconds;
-
       console.log("클린타임");
       console.log(this.cleanTime[0]);
      
